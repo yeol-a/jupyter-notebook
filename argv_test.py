c68976324f2ead4d4578c47fe0e7037011d35054
DESKTOP-0M1SFST\user <kay061476@gmail.com>
2022-10-12 12:48:16 +0900

    first

---
+++ argv_test.py
@@ -0,0 +1,8 @@
+import sys
+print(sys.argv)
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
+print(sys.argv[4])
+print(sys.argv[5])
---
+++ mod2.py
@@ -0,0 +1,7 @@
+P1=3.141592
+class Math:
+	def solv(self, r):
+		return P1 *(r**2)
+
+def add(a, b):
+	return a + b
